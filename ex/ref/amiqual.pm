a7c688a9f67533b4b539d42f2570edc80304ed58
Nic Volanschi <eugene.volanschi@inria.fr>
2019-01-07 14:37:09 +0100

    add operators wsince & until

--- ex/ref/amiqual.pm
+++ ex/ref/amiqual.pm
@@ -8,7 +8,7 @@
 "shower",
 [&lt(180000), &between("C21",[&or, "Eau_Froide_Douche_Instantanee", "Eau_Chaude_Douche_Instantanee"])],
 "cook",
-&occ(&any_up("C3","C14","C15","C16","C17"),&any("Intensite_Four","Intensite_Hote","Intensite_Plaques")),
+&occ(&any_up("C3","C14","C15","C16","C17"),[&or, "Intensite_Four", [&or, "Intensite_Hote", "Intensite_Plaques"]]),
 "sleep",
 [&ge(3600000), "bed_pressure"],
 "toilet",
